8f51a87967c47c6d82da65efee3c733c29a15705
Ankush Khanna <a.khanna@rebuy.de>
2015-12-21 13:53:14 +0100

    Kmeans fix.

--- start2/src/main/scala-2.10/start.scala
+++ start2/src/main/scala-2.10/start.scala
@@ -1,3 +1,5 @@
+import org.apache.spark.{SparkContext, SparkConf}
+
 /**
   * Created by Nafiseh on 11/12/15.
   */
@@ -5,6 +7,12 @@ object start {
 
 
   def main(args: Array[String]) {
-    val kmean = new KMeans
+    val conf = new SparkConf() //
+      .setMaster("local[1]")
+      .setAppName("KMean-test")
+      .set("spark.driver.cores", "3")
+
+    val sc = new SparkContext(conf)
+    val kmean = new KMeans(sc)
   }
 }
